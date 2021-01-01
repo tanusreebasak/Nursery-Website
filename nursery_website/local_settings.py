c5474af337ed9bf7985203d6ef1a7df6075161a7
Tanusree Basak <tanusreebasak16@gmail.com>
2021-01-02 00:02:32 +0530

    Added files merging to master

--- nursery_website/local_settings.py
+++
@@ -1,9 +0,0 @@
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file)))
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
