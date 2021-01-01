63d5501e2d57e4174e5305019ebb5f2534686c51
Tanusree Basak <tanusreebasak16@gmail.com>
2021-01-01 22:56:46 +0530

    Hello

---
+++ firstpage/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home,name="home"),    
+    path('cart/', views.cart,name="cart"),
+    path('adminPlants/', views.addPlant,name="adminPlants"),
+    path('check-out/', views.checkout,name="check-out"),
+    path('orders/', views.orders,name="orders"),
+    path('plants/', views.plants,name="plants"),
+    path('customer/<str:pk>', views.customer,name="customer"),
+    path('create_order/<str:pk>/', views.createOrder,name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder,name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder,name="delete_order"),
+    path('register/', views.registerPage , name="register"),
+    path('login/adminplants', views.loginPage , name="login"),
+    path('logout/', views.logoutUser , name="logout"),    
+    path('account/', views.accountSettings , name="account"),
+] 
